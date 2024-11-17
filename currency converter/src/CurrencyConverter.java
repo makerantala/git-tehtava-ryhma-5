353d610c34a4c7d681097d62fe065ce2e6eefd84
Kim Erwe <kim.erwe@student.hamk.fi>
2024-11-17 16:27:33 +0200

    Added comments

--- currency converter/src/CurrencyConverter.java
+++ currency converter/src/CurrencyConverter.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 
 public class CurrencyConverter {
-    private static final double EURO_TO_USD_RATE = 1.05;
+    private static final double EURO_TO_USD_RATE = 1.05; //Changed the name to CurrencyConverter, also added exchange rates for EURO and DOLLAR.
     private static final double USD_TO_EURO_RATE = 0.95;
 
     public static void main(String[] args) throws Exception {
         
-        Scanner scanner = new Scanner(System.in); 
+        Scanner scanner = new Scanner(System.in); //Variables determined.
 
-        
+
+
     }
 }
