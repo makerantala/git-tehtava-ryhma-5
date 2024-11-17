59c20d69cb2cfcb63a5113c5ffa2b2d373d5d610
Kim Erwe <kim.erwe@student.hamk.fi>
2024-11-17 16:18:18 +0200

    Variables determined and also the rates determined.

---
+++ currency converter/src/CurrencyConverter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class CurrencyConverter {
+    private static final double EURO_TO_USD_RATE = 1.05;
+    private static final double USD_TO_EURO_RATE = 0.95;
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in); 
+
+        
+    }
+}
